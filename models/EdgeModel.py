160fee7dc6cccd1fd9aca36996f6678f9757dcbe
TobiasRoeddiger <roeddiger@teco.edu>
2021-11-12 19:16:58 +0100

    added parameter builder

---
+++ models/EdgeModel.py
@@ -0,0 +1,20 @@
+class EdgeModel:
+    @staticmethod
+    def get_hyperparameters():
+        return None # todo: parameter builder
+
+    def __init__(self, hyperparameters):
+        self.name
+        self.is_fit = False
+        
+    def fit(X, y):
+        return
+    
+    def predict(X):
+        return
+
+    def compile():
+        if (not self.is_fit):
+            return
+            # TODO: throw error
+        

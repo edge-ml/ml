10b885f1a508b777e7684740dd6eaae10fb5655a
KtrauM <murat.kurnaz12@gmail.com>
2021-12-05 20:53:12 +0300

    added new routes

--- routers/models.py
+++ routers/models.py
@@ -22,6 +22,7 @@ edge_models = [
     }
 ]
 
+# Get list of models that can be trained
 @router.get("/models/")
 async def models():
     print(type(EdgeModel).__name__)
@@ -29,7 +30,28 @@ async def models():
 
     return edge_models
 
-
+# Train a model based on the edge_model matching the model_id and given set of hyperparameters
 @router.post("/models/{model_id}")
 async def models_train():
     return "id of the model to train" #TODO return the trained model
+
+# Return a list of models that were trained before by the user
+@router.get("/models/userCreated") # userTrained / trained / created 
+async def models_userCreated():
+    return "List of models"
+
+# Get the perfomance metrics of a trained model
+@router.get("/models/{model_id}/metrics")
+async def models_metrics():
+    return "Performance metrics"
+
+# Get a list of platforms the model is available to.
+@router.get("/models/{model_id}/platforms")
+async def models_platforms():
+    return "Platforms the model is available to"
+
+# Export a model
+@router.get("/models/{model_id}/export")
+async def models_export():
+    return "Model export..."
+

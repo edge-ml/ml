a4b7d194a97da93e133e3552ece93bb8ee72d372
Ömer Erdinç Yağmurlu <omeryagmurlu@gmail.com>
2022-03-25 20:00:16 +0100

    added WIP but working code generation for python

--- app/routers/deploy/deploy.py
+++ app/routers/deploy/deploy.py
@@ -2,33 +2,67 @@ from typing import List
 from fastapi import APIRouter, Depends, Request, Response
 
 from app.db.models import get_model
-from .dependencies import deployment_authorized, deployment_unauthorized
-from app.db.deployments import delete_deployment as db_delete_deployment, rename_deployment as db_rename_deployment
+from app.db.models.model import Model
+from app.models.edge_model import EdgeModelPlatform
+from app.routers.dependencies import validate_model
 
 router = APIRouter()
 
-##### Unauthorized
-
-@router.get("/{deployment_key}")
-async def deployment(deployment=Depends(deployment_unauthorized)):
+@router.get("/{model_id}")
+async def deployment(model=Depends(validate_model)):
     return {
-        'name': deployment.name,
-        'key': deployment.key,
-        'creation_date': deployment.creation_date,
+        'platforms': model.edge_model.get_platforms(),
     }
 
-# TODO: how to deal with platform??
-@router.get("/{deployment_key}/export/{platform}")
-async def download_deployment_model(platform: str, deployment=Depends(deployment_unauthorized)):
-    model = await get_model(deployment.model_id)
+@router.get("/{model_id}/export/{platform}")
+async def export(platform: str, model: Model = Depends(validate_model)):
+    timeseries = model.timeseries if model.timeseries else ["Sensor_A", "Sensor_B", "Sensor_C"]
+    labels = model.labels if model.labels else []
+    wsize =  model.window_size if model.window_size else "<WINDOW_SIZE>"
+    
+    if (EdgeModelPlatform.from_str(platform) == EdgeModelPlatform.PYTHON):
+        tadd = ""
+        for ts in timeseries:
+            tadd = tadd + '\n\tp.add_datapoint(\'{ts}\', get{ts}())'.format(ts = ts)
+
+        code = """import time
+from edgeml.predictor import Predictor, PredictorError
+import pickle
+
+model = # pickle.loads(...) # load model file with pickle
+
+p = Predictor(
+    model,
+    {timeseries_list},
+    {wsize},
+    {labels}
+)
+
+while True:
+{timeseries_add}
+
+    try:
+        print(p.predict())
+    except PredictorError:
+        pass
+    
+    time.sleep(0.25)
+""".format(
+            wsize=wsize,
+            timeseries_list=str(timeseries),
+            labels=str(labels),
+            timeseries_add=tadd
+        )
+
+        return {
+            'code': code,
+            'model': True
+        }
+
+    return {
+        'code': model.edge_model.export(EdgeModelPlatform.from_str(platform))
+    }
+
+@router.get("/{model_id}/download")
+async def dlmodel(model=Depends(validate_model)):
     return Response(content=model.pickled_edge_model, media_type="application/octet-stream")
-
-##### Authorized
-
-@router.delete("/{deployment_key}")
-async def delete_deployment(deployment=Depends(deployment_authorized)):
-    return await db_delete_deployment(deployment.key)
-
-@router.put("/{deployment_key}/name")
-async def rename_deployment(request: Request, deployment=Depends(deployment_authorized)):
-    return await db_rename_deployment(deployment.key, await request.body())

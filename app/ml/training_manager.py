fea26b7322e70bc41dfbfd054b4b2b157ed38c62
KtrauM <murat.kurnaz12@gmail.com>
2022-05-16 09:22:02 +0200

    added timer logic

--- app/ml/training_manager.py
+++ app/ml/training_manager.py
@@ -40,39 +40,48 @@ class TrainingManager:
     # TODO: implement caching for each intermeditary variable
     def start(self, t: Trainer):
         self.trainers[t.id] = t
-
+        time_left = 35
         t.training_state = TrainingState.TRAINING_INITIATED
         
-        print("PHASE 1")
-        initiate_training = self.pool.schedule(t.get_df_labeled_each_dataset, timeout=50)
-        print('PHASE 1 SEC')
+        print("PHASE 1 Begin")
+        start = time.perf_counter()
+        initiate_training = self.pool.schedule(t.get_df_labeled_each_dataset, timeout=time_left)
         try:
             labels, df_labeled_each_dataset = initiate_training.result()
         except TimeoutError:
             print("Task took too long in phase 1")
             self.handle_timeout(t)
             return
+        time_left -= time.perf_counter() - start
+        print('PHASE 1 End: ', time_left)
         
         print("PHASE 2")
+        start = time.perf_counter()
         t.training_state = TrainingState.FEATURE_EXTRACTION
-        feature_extraction = self.pool.schedule(t.feature_extraction, args=[df_labeled_each_dataset], timeout=50)
+        feature_extraction = self.pool.schedule(t.feature_extraction, args=[df_labeled_each_dataset], timeout=time_left)
         try:
             data_x, data_y = feature_extraction.result()
         except TimeoutError:
             print("Task took too long in phase 2")
             self.handle_timeout(t)
             return
-        
+        time_left -= time.perf_counter() - start
+        print('PHASE 2 End: ', time_left)
+
         print("PHASE 3")
+        start = time.perf_counter()
         t.training_state = TrainingState.MODEL_TRAINING
-        training = self.pool.schedule(t.train, args=[data_x, data_y], timeout=50)
+        training = self.pool.schedule(t.train, args=[data_x, data_y], timeout=time_left)
         try:
             model, metrics = training.result()
         except TimeoutError:
             print("Task took too long in phase 3")
             self.handle_timeout(t)
             return
+        time_left -= time.perf_counter() - start
+        print('PHASE 3 End: ', time_left)
 
+        # print(time.perf_counter() - start)
         t.training_state = TrainingState.TRAINING_SUCCESSFUL
         
         asyncio.run(add_model(Model(

0ec5cb0136f87fb9e6ddf726bcaa538faf92d319
Tobias King <tk.king.tk@gmail.com>
2022-08-24 20:13:19 +0200

    Added c code generation

--- app/codegen/inference/CPlatform.py
+++ app/codegen/inference/CPlatform.py
@@ -10,4 +10,15 @@ class CPlatform(BasePlatform):
         return [InferenceFormats.C, InferenceFormats.C_EMBEDDED]
 
     def codegen(self, window_size, timeseries, labels, format, scaler):
-        return "here's c code"
+        temp = []
+        temp.append(f'#include "___DOWNLOADED_MODEL_BASENAME___.h"')
+        temp.append("")
+        temp.append("void loop() {")
+        for t in timeseries:
+            temp.append(f'   add_datapoint("{t}", get{t}());')
+        
+        temp.append("   int predicted_class = predict();")
+        temp.append("   const char* label = class_to_label(predicted_class);")
+        temp.append('   printf("%c\\n", label);')
+        temp.append("}")
+        return "\n".join(temp)

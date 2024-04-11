e1a28572e077a210b33ed98a30c85e729a01c434
Tobias King <tk.king.tk@gmail.com>
2024-04-11 15:28:04 +0200

    Added config for local docker deployment

--- app/StorageProvider/S3StorageProvider.py
+++ app/StorageProvider/S3StorageProvider.py
@@ -9,6 +9,7 @@ class S3StorageProvider(BaseStorageProvider):
 
     def __init__(self):
         self.s3 = boto3.client(service_name='s3', endpoint_url=S3_URL, aws_access_key_id=S3_ACCESS_KEY, aws_secret_access_key=S3_SECRET_KEY)
+        
         if not self.s3.head_bucket(Bucket=S3_MODEL_BUCKET_NAME):
             self.s3.create_bucket(Bucket=S3_MODEL_BUCKET_NAME)
 

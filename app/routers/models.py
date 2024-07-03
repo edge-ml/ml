57b3474721ede8af9b65e38bb2c556a352846926
Tobias King <tk.king.tk@gmail.com>
2024-07-03 12:17:13 +0200

    Added new route to get model by id

--- app/routers/models.py
+++ app/routers/models.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Header, Response
 from app.utils.jsonEncoder import JSONEncoder
-from app.db.models import get_project_models, delete_model
+from app.db.models import get_project_models, delete_model, get_model
 from app.ml.Exporter import download_model
 from app.utils.PyObjectId import PyObjectId
 
@@ -9,6 +9,11 @@ import json
 router = APIRouter()
 
 
+@router.get("/{model_id}")
+async def get_model_by_id(model_id, project: str = Header(...)):
+    res = await get_model(model_id, project)
+    res = res.dict(by_alias=True)
+    return Response(json.dumps(res, cls=JSONEncoder), media_type="application/json")
 
 @router.get("/")
 async def get_models(project: str = Header(...)):
